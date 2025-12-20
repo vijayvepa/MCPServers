556b66600e6341894d95d82a8c826d7f95178ecb
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-12-20 13:41:49 -0500

    Setup env vars

--- SSEServer/README.md
+++ SSEServer/README.md
@@ -19,3 +19,18 @@ uv add langchain-mcp-adapters langchain-openai langgraph python-dotenv
 ```shell
 uv run main.py
 ```
+
+## Online API Keys
+
+- [LangChain Smith](https://smith.langchain.com/)
+- [Open API](https://platform.openai.com/api-keys)
+
+## Environment Variables
+
+In your user directory .bashrc or .zshrc; on in windows env vars
+
+```shell
+export LANGCHAIN_API_KEY="your_langchain_smith_key"
+export OPENAI_API_KEY="your_openai_key"
+```
+
--- SSEServer/main.py
+++ SSEServer/main.py
@@ -1,6 +1,22 @@
-def main():
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+def print_env(key):
+    print(f'{key}: {os.getenv(key)}')  # Print the value of LANGCHAIN_API_KEY
+
+print_env("OPEN_AI_API_KEY")  # Print the value of OPENAI_API_KEY
+print_env("LANGCHAIN_API_KEY")  # Print the value of OPENAI_API_KEY
+print_env("LANGCHAIN_TRACING_V2")  # Print the value of OPENAI_API_KEY
+print_env("LANGCHAIN_ENDPOINT")  # Print the value of OPENAI_API_KEY
+print_env("LANGCHAIN_PROJECT")  # Print the value of OPENAI_API_KEY
+
+
+async def main():
     print("Hello from sseserver!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
